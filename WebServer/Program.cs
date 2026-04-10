18ef768e0c43cf94834d806ab6351a760f5c1f13
K1eranza <zubrevaevelina20641@gmail.com>
2026-04-10 11:31:36 +0300

    Step 8: middleware logging and custom headers

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -3,10 +3,40 @@ using System.Text.RegularExpressions;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    var key = context.Request.Query["key"];
+
+    if (key != "secret")
+    {
+      
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Unauthorized");
+        return; 
+    }
+
+   
+    await next(context);
+});
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[LOG]{context.Request.Method} {context.Request.Path}");
+    await next();
+    Console.WriteLine($"[LOG] Ответ отправлен:{context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Powered-By", "ASP.Net Core Lab 27");
+    await next();
+});
+
+
 app.MapGet("/", () => "Добро пожаловать на сервер");
 app.MapGet("/about", () => "Это мой ASP.NET core сервер");
-app.MapGet("/time", () => "Время на сервере: {DateTime.Now}");
-app.MapGet("/hello/{name}", (string name) => $"Привет,{name}!");
+app.MapGet("/time", () => $"Время на сервере: {DateTime.Now}");
+app.MapGet("/hello/{name}", (string name) => $"Привет, {name}!");
 
 app.MapGet("/student", () => new
 {
@@ -16,23 +46,20 @@ app.MapGet("/student", () => new
     IsActive = true
 });
 
-
 app.MapGet("/subjects", () => new[]
 {
     "РПМ",
     "РМП",
     "ИСРПО",
-"СП",
+    "СП",
 });
 
 app.MapGet("/product/{id}", (int id) => new Product(
-id: id,
-Name: $"Товар  #{id}",
-Price: id * 99.99m,
-InStock: id % 2 == 0
+    id: id,
+    Name: $"Товар  #{id}",
+    Price: id * 99.99m,
+    InStock: id % 2 == 0
 ));
-
-
 
 app.Run();
 
